5e127168d06e5f3912f819b8052ac6a158f61eb9
fm12bj <123998163+fm12bj@users.noreply.github.com>
2023-02-23 17:16:01 -0300

    tesxt digitandi sozinho

---
+++ file.js
@@ -0,0 +1,13 @@
+const saida = document.querySelector(".saida");
+
+function digitacao(texto, contador) {
+  if (contador < texto.length) {
+    setTimeout(() => {
+      saida.textContent += texto.charAt(contador);
+      contador++;
+      digitacao(texto, contador);
+    }, 89);
+  }
+}
+
+digitacao("Agende seu HORARIO", 0);
